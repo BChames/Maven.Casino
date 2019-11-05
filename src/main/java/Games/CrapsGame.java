d15ec1950e89c3646a03cb79c18d1c7f07219bef
Julia Evans <julevans93@users.noreply.github.com>
2019-11-05 16:16:43 -0500

    updating Craps auto win/lose

--- src/main/java/Games/CrapsGame.java
+++ src/main/java/Games/CrapsGame.java
@@ -4,4 +4,13 @@ import GameComponents.Dice;
 
 public class CrapsGame{
 Dice dice = new Dice();
+
+public void firstRoll(){
+if (dice.diceToss() == 7 || dice.diceToss() == 11){
+    System.out.println("You Win!");
+}
+else if (dice.diceToss() == 2 || dice.diceToss() == 3 || dice.diceToss() == 12){
+    System.out.println("You Lose!");
+}
+}
 }
