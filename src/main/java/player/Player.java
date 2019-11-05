5c42c2b9cf1913a24e0c9d1a10246994236c1be3
Brandon Chambers <brandonmchambers36@gmail.com>
2019-11-04 22:20:32 -0500

    start MainMenu Class and Player Class

---
+++ src/main/java/player/Player.java
@@ -0,0 +1,28 @@
+package player;
+
+public class Player {
+
+    private String userName;
+    private Double wallet;
+
+    public Player(String userName, Double wallet){
+        this.userName = userName;
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Double getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(Double wallet) {
+        this.wallet = wallet;
+    }
+}
