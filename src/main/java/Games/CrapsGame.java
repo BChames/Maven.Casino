c20a9e2ec607725c33f1dddcf0c8cffbee885749
Julia Evans <julevans93@users.noreply.github.com>
2019-11-06 16:53:48 -0500

    still working on craps updating bet

--- src/main/java/Games/CrapsGame.java
+++ src/main/java/Games/CrapsGame.java
@@ -1,72 +1,73 @@
 package Games;
 
 import GameComponents.Dice;
+import Interfaces.GamblingGame;
+import Interfaces.GamblingPlayer;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import io.zipcoder.casino.MainApplication.Console;
 import jdk.nashorn.internal.ir.WhileNode;
+import player.CrapsPlayer;
+import player.Player;
 
-public class CrapsGame{
+import java.util.Scanner;
+
+public class CrapsGame implements GamblingGame {
     Dice dice = new Dice(2);
+    CrapsPlayer crapsPlayer = new CrapsPlayer();
+    double betWinnings;
+    double wager;
+    double winnings;
+    double loss;
 
     public void startCraps() {
-            String start = Console.getStringInput("Welcome to Craps! \n Press enter to begin game");
+        //String addBet = Console.getStringInput("Welcome to Craps! \n Enter the amount you want to bet.");
+        addToBet();
+            //String start = Console.getStringInput("Press enter to begin game");
             firstRoll();
         }
+    @Override
+    public Double addToBet() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to Craps! How much would you like to wager?");
+
+        wager = scan.nextDouble();
+        System.out.println("You have chosen to wager $"+wager+", Good luck!");
+
+        return null;
+    }
+    @Override
+    public Double payOut() {
+        return null;
+    }
 
     public void firstRoll(){
         String firstRoll = Console.getStringInput("Press enter to roll the dice");
         int roll = dice.tossAndSum();
         System.out.println(""+roll);
         if (roll == 7 || roll == 11){
-            System.out.println("You rolled a "+roll+". You win!");
+            System.out.println("Congratulations! You rolled a "+roll+". You won $"+wager);
         }
         else if (roll == 2 || roll == 3 || roll == 12){
-            System.out.println("You rolled a "+roll+". You lose!");
+            System.out.println("Sorry, you rolled a "+roll+". You lost $"+wager);
         }
         else {
             int point = roll;
             Boolean keepPlaying = true;
-
-
-
                 String secondRoll = Console.getStringInput("You rolled a " + roll + ". Press enter to roll the dice.");
-
                 //Match the point to win
             while (keepPlaying) {
                 int roll2 = dice.tossAndSum();
-                //int roll3 = dice.tossAndSum();
                 if (roll2 == point) {
-                    System.out.println("You rolled a " + roll2 + " and matched your point. You win!");
+                    System.out.println("Congratulations! You rolled a " + roll2 + " and matched your point. You won $"+wager);
                     keepPlaying = false;
                 }
                 if (roll2 == 7) {
-                    System.out.println("You rolled a 7. You lose!");
+                    System.out.println("Sorry ,you rolled a 7. You lost $"+wager);
                     keepPlaying = false;
                 }
                 if (roll2 != point){
-                    // roll3 = dice.tossAndSum();
                     String keepRolling = Console.getStringInput("You rolled a " + roll2 + ". Press enter to roll the dice.");
-                    /*
-                    if (roll3 == point){
-                        System.out.println("You rolled a " + roll3 + " and matched your point. You win!");
-                        keepPlaying = false;
-                    }
-                    */
                 }
-
-                //Did they win? (roll a 7)
-
-                /*
-                if (roll3 == point){
-                    System.out.println("You rolled a"+roll3+" and matched your point. You win!");
-                    keepPlaying = false;
-                }
-                else if (roll3 == 7){
-                    System.out.println("You rolled a 7. You lose!");
-                    keepPlaying = false;
-                }
-                */
-
             }
         }
     }
--- src/main/java/player/CrapsPlayer.java
+++ src/main/java/player/CrapsPlayer.java
@@ -1,5 +1,20 @@
 package player;
 
-public class CrapsPlayer {
+    public class CrapsPlayer {
+        public Double wallet = 300.0;
+        public Double getWallet() {
+            return wallet;
+        }
 
-}
+        public void setWallet(Double wallet) {
+            this.wallet = wallet;
+        }
+
+
+    /*public CrapsPlayer(Double wallet){
+
+    }
+        public CrapsPlayer() {
+
+        }*/
+    }
