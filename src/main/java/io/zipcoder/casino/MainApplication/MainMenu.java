1bd5acaf048e2536fa9e285103f0a6399ba2eb0b
kendrang <kendrang@users.noreply.github.com>
2019-11-07 15:03:21 -0500

    got betting working properly carries over into main

--- src/main/java/io/zipcoder/casino/MainApplication/MainMenu.java
+++ src/main/java/io/zipcoder/casino/MainApplication/MainMenu.java
@@ -8,37 +8,45 @@ import player.Player;
 public class MainMenu {
 Player casinoPlayer = new Player();
     public void getMainMenu() {
-        Integer input = Console.getIntegerInput(
-                "\nWelcome to the Casino! " +
-                        "\nWhere you don't need money, because all you got is time to spend\n\n" +
-                        "CARD GAMES\n" +
-                            "1. Go Fish\n" +
-                            "2. Blackjack\n" +
-                             "\n" +
-                        "DICE GAMES! \n" +
-                            "3. Craps\n" +
-                            "4. BackAlley Dice\n");
 
-        mainMenuActions(input);
+        mainMenuActions(casinoPlayer);
     }
 
-    private void mainMenuActions(Integer input) {
+    public void mainMenuActions(Player player) {
+        Boolean menuRun = true;
+        while (menuRun) {
+            Integer input = Console.getIntegerInput(
+                    "\nWelcome to the Casino! " +
+                            "\nWhere you don't need money, because all you got is time to spend\n\nYour current balance is: $" + casinoPlayer.getWallet() +
+                            "\n\nCARD GAMES\n" +
+                            "1. Go Fish\n" +
+                            "2. Blackjack\n" +
+                            "\n" +
+                            "DICE GAMES! \n" +
+                            "3. Craps\n" +
+                            "4. BackAlley Dice\n" +
+                            "5. Exit Casino");
 
-        switch (input) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                CrapsGame craps  = new CrapsGame();
-                craps.startCraps();
-                break;
-            case 4:
-                BackAlleyDiceGame backalley = new BackAlleyDiceGame(casinoPlayer);
-                backalley.startBackAlley();
-                break;
-            default:
-                Console.println("\nInvalid selection. Please try again.");
+            switch (input) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    CrapsGame craps = new CrapsGame();
+                    craps.startCraps();
+                    break;
+                case 4:
+                    BackAlleyDiceGame backalley = new BackAlleyDiceGame(player);
+                    backalley.startBackAlley();
+                    break;
+                case 5:
+                    Console.println("Thanks for coming! ");
+                    menuRun = false;
+                    break;
+                default:
+                    Console.println("\nInvalid selection. Please try again.");
+            }
         }
     }
 }
